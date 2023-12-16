f6355d20008f2dcb17b72eadebaa19570d7ffdf6
Jaime <jaime.diaz@patientpoint.com>
2023-12-15 22:06:33 -0600

    feat: remove logtail

--- support/logger.js
+++ support/logger.js
@@ -1,21 +1,21 @@
-const { Logtail } = require("@logtail/node");
+// const { Logtail } = require("@logtail/node");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const logtail = new Logtail(process.env.LOGTAIL_TOKEN);
+// const logtail = new Logtail(process.env.LOGTAIL_TOKEN);
 
 function logger(reference) {
   return {
     info: (msg, payload) => {
       console.log(reference, msg, payload);
-      logtail.info(`${reference}:${msg}`, payload);
+      // logtail.info(`${reference}:${msg}`, payload);
     },
     error: (msg, payload) => {
       console.log(reference, msg, payload);
-      logtail.error(reference, msg);
+      // logtail.error(reference, msg);
     },
     flush: () => {
-      logtail.flush();
+      // logtail.flush();
     },
   };
 }
